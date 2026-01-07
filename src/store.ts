abb5c65a0ef7e941d5743d86f121ab8fa96b3e3f
hu de yi <mehudeyi@163.com>
2026-01-07 15:44:42 +0800

    use idb-keyval instead of localForage

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,36 @@
 // @ts-ignore
-import localforage from './localforage.js';
+// import localforage from './localforage.js';
+import { get, set, createStore } from 'idb-keyval';
+const STORE_NAME = 'maptalks.tileclip.store';
+const DBSTORE = createStore(STORE_NAME, 'tiles');
 
-let tempStore;
+// function getStore() {
+//     if (!tempStore) {
+//         tempStore = localforage.createInstance({
+//             name: 'maptalks.tileclip',
+//             storeName: 'tiles',
+//             description: 'Tile storage for maptalks.tileclip'
+//         });
+//     }
+//     return tempStore;
+// }
 
-function getStore() {
-    if (!tempStore) {
-        tempStore = localforage.createInstance({
-            name: 'maptalks.tileclip',
-            storeName: 'tiles',
-            description: 'Tile storage for maptalks.tileclip'
-        });
-    }
-    return tempStore;
-}
+// export function saveStoreTile(url: string, data: ImageBitmap | ArrayBuffer) {
+//     getStore().setItem(url, data)
+//         .then(() => {
+//             // console.log(`Tile saved: ${url}`);
+//         }).catch((error) => {
+//             console.error(`Error saving tile: ${url}`, error);
+//         });
+// }
+
+// export function getStoreTile(url: string): Promise<ImageBitmap | ArrayBuffer | null> {
+//     return getStore().getItem(url)
+// }
+
 
 export function saveStoreTile(url: string, data: ImageBitmap | ArrayBuffer) {
-    getStore().setItem(url, data)
+    set(url, data, DBSTORE)
         .then(() => {
             // console.log(`Tile saved: ${url}`);
         }).catch((error) => {
@@ -24,5 +39,5 @@ export function saveStoreTile(url: string, data: ImageBitmap | ArrayBuffer) {
 }
 
 export function getStoreTile(url: string): Promise<ImageBitmap | ArrayBuffer | null> {
-    return getStore().getItem(url)
+    return get(url, DBSTORE)
 }
